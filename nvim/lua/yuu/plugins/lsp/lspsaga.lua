c41d88982b2af04f6095e36a3edd51c0fb8c54f5
Nitesh Kumar Tudu <50450044+Nitesh000@users.noreply.github.com>
2023-01-18 12:41:41 +0530

    migrating the lspsaga
    new version of lsp sage is there so i migrated from the older one..

--- nvim/lua/yuu/plugins/lsp/lspsaga.lua
+++ nvim/lua/yuu/plugins/lsp/lspsaga.lua
@@ -4,15 +4,15 @@ if not saga_status then
 	return
 end
 
-saga.init_lsp_saga({
+saga.setup({
 	-- keybinds for navigation in lspsaga window
-	move_in_saga = { prev = "<C-k>", next = "<C-j>" },
+	scroll_preview = { scroll_up = "<C-k>", scroll_down = "<C-j>" },
 	-- use enter to open file with finder
-	finder_action_keys = {
-		open = "<CR>",
+	finder = {
+		edit = "<CR>",
 	},
 	-- use enter to open file with definition preview
-	definition_action_keys = {
+	definition = {
 		edit = "<CR>",
 	},
 })
