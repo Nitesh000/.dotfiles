a33d45c4d681e45006a828bcc805c2cf684ed134
Nitesh Kumar Tudu <50450044+Nitesh000@users.noreply.github.com>
2023-01-18 12:52:48 +0530

    adding a rust formatter to the Mason configuration

--- nvim/lua/yuu/plugins/lsp/mason.lua
+++ nvim/lua/yuu/plugins/lsp/mason.lua
@@ -39,7 +39,8 @@ mason_null_ls.setup({
 	ensure_installed = {
 		"prettier", -- ts/js formatter
 		"stylua", -- lua formatter
-		"eslint_d", -- ts/js linter
+		"eslint_d", -- ts/js linter		
+		"rustfmt", -- rust formatter
 	},
 	-- auto-install configured formatters & linters (with null-ls)
 	automatic_installation = true,
