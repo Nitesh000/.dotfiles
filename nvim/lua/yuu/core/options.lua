caba282c2c8caeab4f6ab9f3d800157acf397de8
Nitesh Kumar Tudu <50450044+Nitesh000@users.noreply.github.com>
2023-02-28 16:36:30 +0530

    added the scrolloff option

--- nvim/lua/yuu/core/options.lua
+++ nvim/lua/yuu/core/options.lua
@@ -19,6 +19,7 @@ opt.smartcase = true -- if you include mixed case in your search, assumes you wa
 
 -- cursor line
 opt.cursorline = true -- highlight the current cursor line
+opt.scrolloff = 2 -- keep 4 lines above and below cursor when scrolling
 
 -- appearance
 
