4e82c993aca4975b986e389ec5890a2a13c664a5
Nitesh Kumar Tudu <50450044+Nitesh000@users.noreply.github.com>
2023-03-02 17:47:05 +0530

    Update plugins-setup.lua

--- nvim/lua/yuu/plugins-setup.lua
+++ nvim/lua/yuu/plugins-setup.lua
@@ -106,6 +106,9 @@ return packer.startup(function(use)
 
 	-- git integration
 	use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
+		
+	-- toggle-terminal for neovim
+	use({ "akinsho/toggleterm.nvim", tag = "*" })
 
 	if packer_bootstrap then
 		require("packer").sync()
