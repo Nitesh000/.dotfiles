36b407ed778c091bb7c68f4cd20218cffa0aa599
Nitesh000 <ntudu040@gmail.com>
2024-11-04 19:41:42 +0530

    revert: back to folke's dap ui event listeners function

--- .config/nvim/lua/yuu/plugins/lsp/dap.lua
+++ .config/nvim/lua/yuu/plugins/lsp/dap.lua
@@ -20,19 +20,16 @@ return {
 			LogPoint = ".>",
 		}
 
-		dapui.setup()
+		dapui.setup({})
 
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
+		dap.listeners.after.event_initialized["dapui_config"] = function()
+			dapui.open({})
 		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
+		dap.listeners.before.event_terminated["dapui_config"] = function()
+			dapui.close({})
 		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
+		dap.listeners.before.event_exited["dapui_config"] = function()
+			dapui.close({})
 		end
 
 		---- Define colors for DAP icons
