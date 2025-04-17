0de0d8b8240ab6ad3bafee155ed1ca819726d467
Nitesh000 <ntudu040@gmail.com>
2025-04-17 17:33:36 +0530

    feat: updates on lspconfig

--- .config/nvim/lua/yuu/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/yuu/plugins/lsp/lspconfig.lua
@@ -21,11 +21,27 @@ return {
 
 		-- Change the Diagnostic symbols in the sign column (gutter)
 		-- local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
+		local custom_signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+		-- for type, icon in pairs(custom_signs) do
+		-- 	local hl = "DiagnosticSign" .. type
+		-- 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+		-- end
+		vim.diagnostic.config({
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = custom_signs.Error,
+					[vim.diagnostic.severity.WARN] = custom_signs.Warn,
+					[vim.diagnostic.severity.HINT] = custom_signs.Hint,
+					[vim.diagnostic.severity.INFO] = custom_signs.Info,
+				},
+				numhl = {
+					[vim.diagnostic.severity.ERROR] = "",
+					[vim.diagnostic.severity.WARN] = "",
+					[vim.diagnostic.severity.HINT] = "",
+					[vim.diagnostic.severity.INFO] = "",
+				},
+			},
+		})
 
 		-- configure html server
 		lspconfig["html"].setup({
